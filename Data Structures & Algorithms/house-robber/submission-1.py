d883a485ca9b93f7dbbf5abf32ff96549266cbdc
Manohar K <56685489+man0hark@users.noreply.github.com>
2026-09-10 16:44:39 -0500

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        # either rob this house then sum the money from till i-2 and money from this house or skip this house and then money is only till i-1
+        dp = [0]*len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+
+        return dp[-1]
